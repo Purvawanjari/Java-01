b9bd4fade48e8eadaa1807597a90838e40c42aaf
Purva <purvawanjari2002@gmail/com>
2025-05-09 16:43:34 +0530

    Show my file code

---
+++ test.java
@@ -0,0 +1,17 @@
+public main{
+public static void main(String[]args){
+
+    System.out.println(Hello);
+
+    //Following commands wew performed
+    // git init
+    // git status
+    // git add
+    // git status
+
+    System.out.println(World);
+
+      
+}
+
+}
