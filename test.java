51e65b9d8733d8366ae4f25e92fb8ba73b026753
Purva <purvawanjari2002@gmail/com>
2025-05-09 17:59:25 +0530

    code run sucessfully

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public main{
 public static void main(String[]args){
 
-    System.out.println(Hello);
+    System.out.println("Hello");
 
     //Following commands wew performed
     // git init
@@ -9,9 +9,13 @@ public static void main(String[]args){
     // git add
     // git status
 
-    System.out.println(World);
+    System.out.println("World");
+    // git commit -m
+    //  git config --global user.email "you@example.com"
+    //  git config --global user.name "Your Name"
+    //  git log
 
-      
+    System.out.println("Sucessfully run");
 }
 
 }
